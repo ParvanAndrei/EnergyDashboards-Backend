67d7185a410a37ab742df2c4d5ae6c81e04c34ea
Parvan Andrei Leonard <andrei.parvan81@gmail.com>
2024-06-16 18:38:37 +0300

    final push

--- main.py
+++ main.py
@@ -1,15 +1,7 @@
-from fastapi import FastAPI, Header, Depends, Request, Response
-from starlette.responses import RedirectResponse
-from starlette.exceptions import HTTPException as StarletteHTTPException
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 import requests
 from datetime import date
-
-
-
-# from routes import todos
-# from fastapi_keycloak_middleware import KeycloakConfiguration, KeycloakMiddleware
 
 origins = [
     "*",
@@ -17,7 +9,6 @@ origins = [
 ]
 
 app = FastAPI()
-# app.include_router(todos.router)
 
 app.add_middleware(
     CORSMiddleware,
@@ -30,35 +21,6 @@ app.add_middleware(
 @app.get("/accestoken")
 def read_headers(request: Request):
     return dict(request.headers)
-
-# @app.get("/accestoken")
-# # def protected_route(token: str = Depends(get_access_token)):
-# def protected_route(request: Request):
-#     # print("TOKEN HERE ", token)
-#     username = request.headers.get("OIDC_preferred_username")
-#     given_name = request.headers.get("X-User-Given-Name")
-#     family_name = request.headers.get("X-User-Family-Name")
-#     # return {"token": token}
-#     return {"username": username,
-#             "given_name" : given_name,
-#             "familit_name" : family_name
-#             }
-
-# @app.get("/accestoken")
-# def get_access_token(request: Request):
-#     access_token = request.headers.get("OIDC_preferred_username")
-#     print(request.headers)
-#     # family_name = request.headers.get("X-User-Family-Name")
-#     # given_name = request.headers.get("X-User-Given-Name")
-#     # username = request.headers.get("X-User-Username")
-#     # print("THIS IS GIVEN NAME FROM HEADER", given_name)
-#     # print("THIS IS FAMILY NAME FROM HEADER", family_name)
-#     return {
-#         "access_token": access_token
-#         # "family_name": family_name,
-#         # "given_name": given_name,
-#         # "username": username
-#     }
 
 @app.get("/energy-consumption-per-minute")
 def root(start_date, end_date):
